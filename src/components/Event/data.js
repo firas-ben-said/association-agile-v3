dc2a7631cc1a069b0ced2fc3a8523a0155946ba4
atoure94 <ahmad.alkawssar.toure@horizon-tech.tn>
2024-05-05 06:45:06 +0100

    event  dynamic and navbar

---
+++ src/components/Event/data.js
@@ -0,0 +1,22 @@
+const events = [
+    {
+        "imageUrl": "https://placehold.co/600x400",
+        "date": "Mar 16, 2020",
+        "author": "Michael Foster",
+        "description": "Boost your conversion rate"
+    },
+    {
+        "imageUrl": "https://placehold.co/600x400",
+        "date": "Mar 10, 2020",
+        "author": "Lindsay Walton",
+        "description": "How to use search engine optimization"
+    },
+    {
+        "imageUrl": "https://placehold.co/600x400",
+        "date": "Feb 12, 2020",
+        "author": "Tom Cook",
+        "description": "Improve your customer Bonjour"
+    }
+];
+
+export default events;
---
+++ src/components/navbar/links/navLink/NavLink.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./navLink.module.css";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({item}) => {
+
+    const pathname = usePathname();
+
+    return (
+        <Link href={item.path} className={`${styles.container} ${
+            pathname === item.path && styles.active
+        }`}>
+            {item.title}
+        </Link>
+    )
+};
+
+export default NavLink;
