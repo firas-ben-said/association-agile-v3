773948d0432dd92e066fd01bf7c0b54c7de7529a
Firas Ben Said <firas.ben.said@horizon-tech.tn>
2024-05-07 18:43:39 +0100

    6h43 07/05

--- src/app/user/layout.jsx
+++ src/app/user/layout.jsx
@@ -2,21 +2,27 @@ import styles from "@/components/dashboard/dashboard.module.css";
 import Footer from "@/components/dashboard/footer/footer";
 import Navbar from "@/components/dashboard/navbar/navbar";
 import Sidebar from "@/components/dashboard/sidebar/sidebar";
+import { Inter } from "next/font/google";
+import "./user.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 
-const Layout = ({children}) => {
-    return (
+export default function AdminLayout({ children }) {
+  return (
+    <html>
+      <body className={inter.className}>
         <div className={styles.container}>
-            <div className={styles.menu}>
-                <Sidebar />
-            </div>
-            <div className={styles.content}>
-                <Navbar />
-                {children}
-                <Footer />
-            </div>
+          <div className={styles.menu}>
+            <Sidebar />
+          </div>
+          <div className={styles.content}>
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
         </div>
-    )
+      </body>
+    </html>
+  );
 }
-
-export default Layout;
